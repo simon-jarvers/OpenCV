7775850bc5960c512479c88f6576e99e4ea94d17
Simon Jarvers <simon.jarvers@hotmail.de>
2021-07-21 10:10:31 +0200

    draw tracking points

--- lift_motion_tracker.py
+++ lift_motion_tracker.py
@@ -10,11 +10,19 @@ success, img = cap.read()
 bbox = cv2.selectROI("Deadlift", img, False)
 tracker.init(img, bbox)
 
+tracked_points = []
+
+
+def draw_tracked_points(tracked_points):
+    for point in tracked_points:
+        cv2.circle(vid, (point[0], point[1]), 5, (255, 0, 255), cv2.FILLED)
+
 
 def draw_box(vid, bbox):
     x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
     cv2.rectangle(vid, (x, y), (x+w, y+h), (255, 0, 255), 3, 1)
     cv2.putText(vid, "Tracking", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+    return int(x+w/2), int(y+h/2)
 
 
 while True:
@@ -30,7 +38,12 @@ while True:
         success_tracker, bbox = tracker.update(vid)
 
         if success_tracker:
-            draw_box(vid, bbox)
+            x, y = draw_box(vid, bbox)
+            cv2.circle(vid, (x, y), 5, (255, 0, 255), cv2.FILLED)
+            tracked_points.append([x, y])
+            draw_tracked_points(tracked_points)
+            print(x, y)
+
         else:
             cv2.putText(vid, "Lost", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
