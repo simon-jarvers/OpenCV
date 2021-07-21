960acf6b6195ce2dc12101ded88d4bd5a73d5005
Simon Jarvers <simon.jarvers@hotmail.de>
2021-07-21 18:46:18 +0200

    Score max set to 100

--- lift_motion_tracker.py
+++ lift_motion_tracker.py
@@ -1,12 +1,13 @@
 import cv2
 import numpy as np
 
-file_path = "venv/Resources/deadlift.mp4"
+file_path = "venv/Resources/deadlift2.mp4"
 cap = cv2.VideoCapture(file_path)
 
-
-save_size = (int(cap.get(3)), int(cap.get(4)))
-save_video = cv2.VideoWriter('venv/Resources/tracked_deadlift.avi', cv2.VideoWriter_fourcc(*'MJPG'), 30, save_size)
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+save_size = (frame_width, frame_height)
+save_video = cv2.VideoWriter('venv/Resources/tracked_deadlift2.avi', cv2.VideoWriter_fourcc(*'MJPG'), 30, save_size)
 
 tracker = cv2.legacy_TrackerMOSSE.create()
 # tracker = cv2.legacy_TrackerCSRT.create()
@@ -38,7 +39,10 @@ def append_tracked_points(x, y):
     if not tracked_points:
         tracked_points.append([x, y, 0])
     else:
-        diff = y - tracked_points[-1][1]
+        try:
+            diff = y - tracked_points[-2][1]
+        except IndexError:
+            diff = y - tracked_points[-1][1]
         if diff > threshold:  # going down, assign -1
             tracked_points.append([x, y, -1])
             tracked_lifts[current_lift].append([x, y, -1])
@@ -53,25 +57,35 @@ def append_tracked_points(x, y):
                 tracked_lifts.append([])
 
 
-def draw_box(vid, bbox):  # draw the given boung box and return the center
+def draw_box(vid, bbox):  # draw the given bounding box and return the center
     x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
     cv2.rectangle(vid, (x, y), (x+w, y+h), (255, 0, 255), 3, 1)
     cv2.putText(vid, "Tracking", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
     return int(x+w/2), int(y+h/2)
 
 
-def lift_offset(vid, tracked_lifts):
-    if not tracked_lifts[current_lift]:
-        return
+def write_offset_score(vid, tracked_lift):  # display Offset and Score
+    if not tracked_lift:
+        return 0
     else:
-        current_offset = tracked_lifts[current_lift][-1][0] - tracked_lifts[current_lift][0][0]
+        current_offset = tracked_lift[-1][0] - tracked_lift[0][0]
         cv2.putText(vid, "Offset: " + str(current_offset), (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
-        score = 0
-        for lift in tracked_lifts[current_lift]:
-            score += (lift[0] - tracked_lifts[current_lift][0][0])**2/len(tracked_lifts[current_lift])
-        score = round(score, 2)
+        score = lift_score(tracked_lift)
         cv2.putText(vid, "Score: " + str(score), (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+        return score
+
+
+def lift_score(tracked_lift):
+    score = 0
+    mean = np.mean(tracked_lift, axis=0)
+    # for previous, current in zip(tracked_lift, tracked_lift[1:]):
+    #     score += (1 - (current[0] - previous[0]) ** 2) / \
+    #              (abs(current[1] - previous[1]) + 1)
+    for current in tracked_lift:
+        score += (100 - (mean[0] - current[0])**2) / (len(tracked_lift))
+    score = round(score, 2)
+    return score
 
 
 while True:
@@ -90,12 +104,13 @@ while True:
             x, y = draw_box(vid, bbox)
             append_tracked_points(x, y)
             draw_tracked_points(vid, tracked_points)
-            lift_offset(vid, tracked_lifts)
+            write_offset_score(vid, tracked_lifts[-1])
 
         else:
             cv2.putText(vid, "Lost", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
         cv2.imshow("Deadlift", vid)
+        img = vid
         save_video.write(vid)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -104,10 +119,19 @@ while True:
     else:
         break
 
-cap = cv2.VideoCapture(file_path)
-success, img = cap.read()
+# cap = cv2.VideoCapture(file_path)
+# success, img = cap.read()
 draw_tracked_points(img, tracked_points)
-cv2.imshow("Lifts", img)
+# cv2.rectangle(img, (10, 10), (250, frame_height - 10), (255, 255, 255), cv2.FILLED)
+for tracked_lift in tracked_lifts:
+    if len(tracked_lift) < 3:
+        tracked_lifts.remove(tracked_lift)
+cv2.rectangle(img, (10, 10), (250, 30*len(tracked_lifts) + 25), (255, 255, 255), cv2.FILLED)
+for i, tracked_lift in enumerate(tracked_lifts):
+    score = lift_score(tracked_lift)
+    cv2.putText(img, "Score " + str(i + 1) + ": " + str(score), (30, 30*(i+1) + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+cv2.imshow("Deadlift", img)
+save_video.write(img)
 cv2.waitKey(0)
 
 # cap.release()
